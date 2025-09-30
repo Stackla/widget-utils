b8e6eb648704b45e3263ee1fe6df26b7da81eccd
Oliver Kucharzewski <oliver@olidev.com.au>
2025-09-30 16:55:29 +1000

    feat: add support for expanded tile components in loadFeatures function

---
+++ .changeset/purple-berries-pay.md
@@ -0,0 +1,5 @@
+---
+"@stackla/widget-utils": patch
+---
+
+Resolve component loading missing logic
--- src/widget-loader.ts
+++ src/widget-loader.ts
@@ -54,6 +54,19 @@ async function loadFeatures(sdk: ISdk, settings: EnforcedWidgetSettings) {
     sdk.addLoadedComponents(["timephrase"])
   }
 
+  const { show_products, show_add_to_cart, show_carousel_grouping } = sdk.getExpandedTileConfig()
+
+  if (show_carousel_grouping) {
+    sdk.addLoadedComponents(["carousel-grouping"])
+  }
+
+  if (show_products) {
+    sdk.addLoadedComponents(["products", "inline-products"])
+  }
+  if (show_add_to_cart) {
+    sdk.addLoadedComponents(["add-to-cart"])
+  }
+
   if (showTitle) {
     loadTitle(sdk)
   }
